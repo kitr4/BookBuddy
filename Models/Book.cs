e2c827f12faa1024ec628aac26991d1447237f47
kitr4 <jeppeporsing@gmail.com>
2023-11-24 12:25:31 +0100

    h

--- Models/Book.cs
+++ Models/Book.cs
@@ -13,6 +13,8 @@ namespace BookBuddy.Models
         private string _genre;
         private string _title;
         private string _description;
+        private int _year;
+        private string _language;
 
      
         // Properties
@@ -20,16 +22,21 @@ namespace BookBuddy.Models
         public string Title { get => _title; set => _title = value; }
         public string Description { get => _description; set => _description = value; }
         public int BookId { get => _bookId; set => _bookId = value; }
+        public int Year { get => _year; set => _year = value; }
+        public string Language { get => _language; set => _language = value; }
 
 
         // private Author author;
         // Ville det give mening at tage denne med? Hvis vi istedet laver en liste på hver author der creates og tilføjer bog med et hvis ID (hentet fra author_books) og tilføjer bogen til instantiering af author, er det vel ikke nødvendigt at have bogen tilkoblet. private string _author;
-        public Book(string genre, string title, string description, int bookId)
+
+        public Book(int bookId, string genre, string title, string description, int year, string language)
         {
-            BookId = bookId;
+            _bookId = bookId;
             _genre = genre;
             _title = title;
             _description = description;
+            _year = year;
+            _language = language;
         }
     }
 }
